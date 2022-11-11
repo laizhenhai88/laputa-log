42471ad5ac8fb9f266f30e2f18b652ba1b25ecda
laihongyu <hongyusir@foxmail.com>
2022-11-11 22:55:48 +0800

    更新winston版本

--- laputa-log-conf.js
+++ laputa-log-conf.js
@@ -10,34 +10,40 @@ const pname = path.basename(process.cwd())
 const os = require('os')
 
 const errorFormat = format((info, opts) => {
-  if (info instanceof Error) {
-    info.message = info.stack
-  }
-  if (info.error && info.error instanceof Error) {
-    info.message += os.EOL + info.error.stack
-  }
-  return info
+    if (info instanceof Error) {
+        info.message = info.stack
+    }
+    if (info.error && info.error instanceof Error) {
+        info.message += os.EOL + info.error.stack
+    }
+    return info
 })
 
 module.exports = {
-  createLogger: () => {
-    return createLogger({
-      level: 'info',
-      format: format.combine(errorFormat(), format.timestamp(), format.printf((info) => {
-        return `[${info.timestamp}] [${info.level}] - ${info.message}`
-      })),
-      transports: [
-        new transports.Console(),
-        new DailyRotateFile({
-          level: 'error',
-          filename: `./logs/${pname}-${pid}-error-%DATE%.log`,
-          datePattern: 'YYYY-MM-DD',
-          zippedArchive: true,
-          maxSize: '100m',
-          maxFiles: '14d'
-        }),
-        new DailyRotateFile({filename: `./logs/${pname}-${pid}-out-%DATE%.log`, datePattern: 'YYYY-MM-DD', zippedArchive: true, maxSize: '100m', maxFiles: '14d'})
-      ]
-    })
-  }
+    createLogger: () => {
+        return createLogger({
+            level: 'info',
+            format: format.combine(errorFormat(), format.timestamp(), format.printf((info) => {
+                return `[${info.timestamp}] [${info.level}] - ${info.message}`
+            })),
+            transports: [
+                new transports.Console(),
+                new DailyRotateFile({
+                    level: 'error',
+                    filename: `../logs/${pname}/error.%DATE%.log`,
+                    datePattern: 'YYYY-MM-DD',
+                    zippedArchive: false,
+                    maxSize: '1000m',
+                    maxFiles: '3d'
+                }),
+                new DailyRotateFile({
+                    filename: `../logs/${pname}/service.%DATE%.log`,
+                    datePattern: 'YYYY-MM-DD',
+                    zippedArchive: false,
+                    maxSize: '1000m',
+                    maxFiles: '3d'
+                })
+            ]
+        })
+    }
 }
